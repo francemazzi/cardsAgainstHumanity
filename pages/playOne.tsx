dc5e132ce65322fcc143ba5adb07d61a2e167bb8
FraSma <francemazzi@gmail.com>
2023-01-23 18:55:01 +0100

    feat: create play onek

--- pages/playOne.tsx
+++ pages/playOne.tsx
@@ -1,7 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import CardDesk from "../commons/cardsDeks.json";
 import { cardW, cardB } from "../commons/types";
 import Card from "../components/atoms/Card";
+
+interface randomCardInterface {
+  B: {
+    card: cardB;
+    color: string;
+  };
+  W: {
+    cards: cardW[];
+    color: string;
+  };
+}
 
 const blackCards = CardDesk[0].black;
 const whiteCards = CardDesk[0].white;
@@ -14,7 +25,6 @@ const randomiCard = (cardBlack: cardB[], cardWhite: cardW[]) => {
     let randomNumber = Math.floor(Math.random() * cardWhite.length);
     newWhiteArr.push(cardWhite[randomNumber]);
   }
-  console.log(newWhiteArr);
 
   const dataCards = {
     B: { card: randomBCard, color: "black" },
@@ -29,8 +39,12 @@ const selectCard = (e: any) => {
 };
 
 function PlayOne() {
-  const randomcards = randomiCard(blackCards, whiteCards);
-  console.log(randomcards);
+  const [randomicCards, setRandomicCards] = useState<randomCardInterface>();
+
+  useEffect(() => {
+    const randomcards = randomiCard(blackCards, whiteCards);
+    setRandomicCards(randomcards);
+  }, []);
 
   const [select, setSelect] = useState();
 
@@ -40,18 +54,21 @@ function PlayOne() {
         Gioca umano!
       </h1>
       <div className="flex flex-col items-center justify-center">
-        <Card text={randomcards.B.card.text} color={randomcards.B.color} />
+        <Card
+          text={randomicCards?.B.card.text}
+          color={randomicCards?.B.color}
+        />
       </div>
       <h1 className="text-[20px] font-bold w-full text-center text-[white] p-2">
         Scegli dal tuo mazzo e fammi ridere
       </h1>
-      <div className="flex flex-row items-center md:justify-center lg:justify-center sm:justify-start overflow-x-scroll  shadow-md">
-        {randomcards.W.cards.map((data, idk) => {
+      <div className="flex flex-row items-center  justify-start overflow-x-scroll shadow-md">
+        {randomicCards?.W.cards.map((data, idk) => {
           return (
             <div key={idk}>
               <Card
                 text={data.text}
-                color={randomcards.W.color}
+                color={randomicCards?.W.color}
                 // onClick={selectCard}
               />
             </div>
