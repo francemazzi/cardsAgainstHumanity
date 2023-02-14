bd2672346c8710462e2c7b04cce7efff9c3a17ec
FraSma <francemazzi@gmail.com>
2023-02-14 10:55:41 +0100

    bug: playOne doesn't work draNdrop

--- components/atoms/Card/index.tsx
+++ components/atoms/Card/index.tsx
@@ -13,9 +13,9 @@ function Card({ text, color, onClick }: Props) {
       className={`flex flex-col justify-center items-center  m-2  h-[14rem] w-[10rem] md:h-[19rem] md:w-[15rem] lg:h-[20rem] lg:w-[16rem] bg-[${color}] cursor-pointer overflow-hidden shadow-lg rounded-md hover:scale-105 hover:rounded-md transition-all duration-150 aese-out shadow-cyan-500/50`}
     >
       <p
-        className={`text-[${(color = "white"
-          ? "black"
-          : "white")}] font-bold p-2 text-center text-[15px] lg:text-[25px]`}
+        className={`text-[${
+          color == "white" ? "black" : "white"
+        }] font-bold p-2 text-center text-[15px] lg:text-[25px]`}
       >
         {text}
       </p>
--- pages/playOne.tsx
+++ pages/playOne.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import CardDesk from "../commons/cardsDeks.json";
 import { cardW, cardB } from "../commons/types";
 import Card from "../components/atoms/Card";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 interface randomCardInterface {
   B: {
@@ -49,33 +50,59 @@ function PlayOne() {
   const [select, setSelect] = useState();
 
   return (
-    <div className="bg-[black] h-screen">
-      <h1 className="text-[20px] font-bold w-full text-center text-[white] p-2">
-        Gioca umano!
-      </h1>
-      <div className="flex flex-col items-center justify-center">
-        <Card
-          text={randomicCards?.B.card.text}
-          color={randomicCards?.B.color}
-        />
-      </div>
-      <h1 className="text-[20px] font-bold w-full text-center text-[white] p-2">
-        Scegli dal tuo mazzo e fammi ridere
-      </h1>
-      <div className="flex flex-row items-center  justify-start overflow-x-scroll shadow-md">
-        {randomicCards?.W.cards.map((data, idk) => {
-          return (
-            <div key={idk}>
+    <DragDropContext onDragEnd={() => {}}>
+      <div className="bg-[black] h-screen">
+        <h1 className="text-[20px] font-bold w-full text-center text-[white] p-2">
+          Gioca umano!
+        </h1>
+        <Droppable droppableId="characters">
+          {(provided) => (
+            <div
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+              className="flex flex-col items-center justify-center w-full"
+            >
               <Card
-                text={data.text}
-                color={randomicCards?.W.color}
-                // onClick={selectCard}
+                text={randomicCards?.B.card.text}
+                color={randomicCards?.B.color}
               />
             </div>
-          );
-        })}
+          )}
+        </Droppable>
+        <h1 className="text-[20px] font-bold w-full text-center text-[white] p-2">
+          Scegli dal tuo mazzo e fammi ridere
+        </h1>
+        <Droppable droppableId="characters">
+          {(provided) => (
+            <div
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+              className="flex flex-row items-center  justify-start overflow-x-scroll shadow-md"
+            >
+              {randomicCards?.W.cards.map((data, idk) => {
+                return (
+                  <Draggable draggableId={data.text} index={idk} key={idk}>
+                    {(provided) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                      >
+                        <Card
+                          text={data.text}
+                          color={randomicCards?.W.color}
+                          // onClick={selectCard}
+                        />
+                      </div>
+                    )}
+                  </Draggable>
+                );
+              })}
+            </div>
+          )}
+        </Droppable>
       </div>
-    </div>
+    </DragDropContext>
   );
 }
 
